1e42eb6cedcaaf491c2925df19d726529f99cdd8
klippe <konoshenko.vl@gmail.com>
2017-07-14 13:50:15 +0300

    Мероприятия

---
+++ app/src/main/java/dev/klippe/unity/adapter/EventAdapter.java
@@ -0,0 +1,58 @@
+package dev.klippe.unity.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+import dev.klippe.unity.R;
+
+public class EventAdapter extends BaseAdapter {
+    Context ctx;
+    LayoutInflater lInflater;
+    ArrayList<String> objects;
+
+    public EventAdapter(Context context, ArrayList<String> products) {
+        ctx = context;
+        objects = products;
+        lInflater = (LayoutInflater) ctx
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    // кол-во элементов
+    @Override
+    public int getCount() {
+        return objects.size();
+    }
+
+    // элемент по позиции
+    @Override
+    public Object getItem(int position) {
+        return objects.get(position);
+    }
+
+    // id по позиции
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    // пункт списка
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // используем созданные, но не используемые view
+        View view = convertView;
+        if (view == null) {
+            view = lInflater.inflate(R.layout.item, parent, false);
+        }
+
+
+        return view;
+    }
+
+
+
+}
